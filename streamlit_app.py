0bbaa8ce7df35cc8c0349cd23a3974af0ac48cd6
Sameer-Git91 <142480616+Sameer-Git91@users.noreply.github.com>
2023-08-17 13:48:51 +0530

    Update streamlit_app.py
    added filter

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,8 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Let's put a pick list here so they can pick the fruit they want to include only
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+#fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 fruits_to_show=my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
