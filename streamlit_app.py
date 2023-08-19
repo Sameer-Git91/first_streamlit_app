c5e110f1253838fa4411a150aeb1bdc560aa4ad7
Sameer-Git91 <142480616+Sameer-Git91@users.noreply.github.com>
2023-08-19 22:06:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ try:
 # take the json version of the response and normalise it
     #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as a table
-    back_from_function = get_frutyvice_data(fruit_choice)
+    back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
